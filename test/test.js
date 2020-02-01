a590a8389d2c3340a2c50f0c182c2965be9c39e4
Jason De Oliveira <jason.deoliveira@oodrivegroup.net>
2020-02-01 13:50:30 +0100

    revert

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world2', function(done) {
- request(app).get('/').expect('hello world2', done);
+ request(app).get('/').expect('hello world', done);
  });
  app.stop();
 });

4ba0e5d99b3c7adcabc6bb51e9e0380033e06e97
Jason De Oliveira <jason.deoliveira@oodrivegroup.net>
2020-02-01 13:35:21 +0100

    Test

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-//importing node framework
-var express = require('express');
- 
-var app = express();
-//Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
- res.send('hello world');
- //res.send('world');
-});
-//listen to port 3000 by default
-let server = app.listen(process.env.PORT || 5000);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-function stop() {
-    server.close();
-  }
-
-module.exports = server;
-module.exports.stop = stop;
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
+  .get('/', (req, res) => res.send('hello world'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

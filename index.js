f01b5be7510e44374930627a782bc159f151425a
Jason De Oliveira <jason.deoliveira@oodrivegroup.net>
2020-02-01 13:46:35 +0100

    done

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.send('hello world'))
-  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
   let server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   function stop() {

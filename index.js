751a1baa8eda7f362d7c82191dc12b0754abe6a0
Jason De Oliveira <jason.deoliveira@oodrivegroup.net>
2020-02-01 14:10:42 +0100

    added other pages

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 5000
 
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send('hello world'))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/test', (req, res) => res.send('hello world'))
   let server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   function stop() {
